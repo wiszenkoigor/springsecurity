35e8a8235578bc475e8a3511a2ae5779e463c2be
ivivix90 <wiszenkoigor@gmail.com>
2022-10-18 14:24:51 +0200

    permision based auth implemented

--- src/main/java/com/example/springsec/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsec/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ package com.example.springsec.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -33,6 +34,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,19 +50,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())    // ROLE_STUDENT
+      //          .roles(STUDENT.name())    // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthories())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())         // ROLE_ADMIN
+       //         .roles(ADMIN.name())         // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthories())
                 .build();
 
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMINTRAINEE.name())          // ROLE_ADMINTRAINEE
+      //          .roles(ADMINTRAINEE.name())          // ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthories())
                 .build();
 
 
--- src/main/java/com/example/springsec/security/ApplicationUserRole.java
+++ src/main/java/com/example/springsec/security/ApplicationUserRole.java
@@ -1,8 +1,11 @@
 package com.example.springsec.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.springsec.security.ApplicationUserPermission.*;
 
@@ -19,6 +22,14 @@ public enum ApplicationUserRole {
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthories() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
 }
